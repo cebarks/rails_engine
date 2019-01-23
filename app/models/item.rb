aa12b7df3435974437143620f6c95f949056599a
Anten Skrabec <cebarks@gmail.com>
2019-01-23 12:17:13 -0700

    adds validations and associations to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :description
+  validates_numericality_of :unit_price
+  
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end

660aba8100410a6f748f9c4f7466d0c7689bf5ae
Anten Skrabec <cebarks@gmail.com>
2019-01-27 12:40:28 -0700

    fixes N+1 query

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::InvoiceItemsController < ApplicationController
   def index
-    render json: InvoiceItemSerializer.new(InvoiceItem.all)
+    render json: InvoiceItemSerializer.new(InvoiceItem.all.includes(:items, :invoices))
   end
 
   def show
-    render json: InvoiceItemSerializer.new(InvoiceItem.find(params[:id]))
+    render json: InvoiceItemSerializer.new(InvoiceItem.find(params[:id]).includes(:items, :invoices))
   end
 end
